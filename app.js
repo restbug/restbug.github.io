c2462770eb197edc7edd5ff62f2c1b931984e534
Li Zhe <lizhe@lizhe.org>
2017-05-31 03:37:48 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        method: 'GET',
+        url: '',
+        headers: '{}',
+        body: '',
+        resStatusCode: '',
+        resHeaders: '',
+        resBody: ''
+    },
+    methods: {
+        sendRequest: sendRequest
+    }
+});
+
+function getProxyId(hostname, callback) {
+    jQuery.get('https://restbug.azurewebsites.net/api/getProxy?hostname=' + hostname, function(res) {
+        var proxyId = res.proxyId;
+        callback(proxyId);
+    });
+}
+
+function sendRequest() {
+    var url = app.url;
+    var method = app.method;
+    var urlParser = document.createElement('a');
+    urlParser.href = url;
+    var hostname = urlParser.host;
+    var rest = urlParser.pathname + urlParser.search;
+    urlParser = null;
+    var headers;
+    try {
+        headers = JSON.parse(app.headers);
+    } catch(e) {
+        headers = {};
+    }
+    var body = app.body || null;
+    getProxyId(hostname, function(proxyId) {
+        var url = 'https://restbug.azurewebsites.net/proxy/' + proxyId + rest;
+        jQuery.ajax({
+            url: url,
+            method: method,
+            headers: headers,
+            data: body
+        }).done(function(data, textStatus, jqXHR) {
+            app.resStatusCode = jqXHR.status;
+            app.resHeaders = jqXHR.getAllResponseHeaders();
+            app.resBody = jqXHR.responseText;
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            app.resStatusCode = jqXHR.status;
+            app.resHeaders = jqXHR.getAllResponseHeaders();
+            app.resBody = jqXHR.responseText;
+        });
+    });
+}
